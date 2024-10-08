18ae56a8fbdd0226c85a00dbe463b2a649d36b0d
OxtailSooop <really@chalkydogshit.co.uk>
2024-10-08 18:09:21 +0100

    Moved redeem messages to a json file

--- bot/cmds/redeem.py
+++ bot/cmds/redeem.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 import discord
 from discord import app_commands
@@ -13,13 +14,12 @@ class Redeem(commands.Cog):
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     async def redeem(self, interaction: discord.Interaction, code: str):
-        responses = ["MA'AM, DO NOT REDEEM THAT CODE!", "WHY? DID YOU REDEEM IT?!", "OH NO, YOU REDEEMED IT! WHAT HAVE YOU DONE?!", "WHY ARE YOU NOT LISTENING TO ME?!", "I'M LITERALLY SHOUTING!! I'M LITERALLY SHOUTING RIGHT NOW MA'AM!!!!", "WAIT... PLEASE WAIT!", "NO!! NO! NO!!!", "WOAH WAIT!", "MA'AM NOOOOOOOOOOOOOOOO!", "NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!", "YOU DON'T HAVE TO DO THAT!!"]
-        invalid = ["Your code could not be redeemed.", "There is a problem with that code, try a different one.", "This code has been redeemed.", "You have already redeemed this code."]
+        responses = json.load(open("./assets/redeem.json", "r"))
         if random.randint(1, 10) == 1:
-            embed = discord.Embed(description=self.client.config["emojis"]["crossOrange"] + " **Error**: " + random.choice(invalid), color=int(self.client.config["colors"]["orange"], 16))
+            embed = discord.Embed(description=self.client.config["emojis"]["crossOrange"] + " **Error**: " + random.choice(responses["invalid"]), color=int(self.client.config["colors"]["orange"], 16))
             await interaction.response.send_message(embed=embed)
         else:
-            await interaction.response.send_message(content=random.choice(responses))
+            await interaction.response.send_message(content=random.choice(responses["responses"]))
 
 async def setup(client):
     await client.add_cog(Redeem(client))
