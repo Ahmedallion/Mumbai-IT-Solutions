012df684b4e784e7cbd3d5002413bd9648cf3b72
Tamim Ahmed <ahmedallion@tamtop.lan>
2024-10-15 22:34:31 +0100

    Updated invalid responses for redeem

--- bot/cmds/redeem.py
+++ bot/cmds/redeem.py
@@ -16,7 +16,7 @@ class Redeem(commands.Cog):
     async def redeem(self, interaction: discord.Interaction, code: str):
         responses = json.load(open("./bot/assets/redeem.json", "r"))
         if random.randint(1, 10) == 1:
-            embed = discord.Embed(description=self.client.config["emojis"]["crossOrange"] + " **Error**: " + random.choice(responses["invalid"]), color=int(self.client.config["colors"]["orange"], 16))
+            embed = discord.Embed(description=self.client.config["emojis"]["crossOrange"] + " " + random.choice(responses["invalid"]), color=int(self.client.config["colors"]["orange"], 16))
             await interaction.response.send_message(embed=embed)
         else:
             await interaction.response.send_message(content=random.choice(responses["responses"]))
